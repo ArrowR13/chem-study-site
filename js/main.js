14d3a21c5ac2ea81175bc03ee62d5c3df0f2868a
ArrowR13 <mmad3.0@gmail.com>
2013-12-13 18:17:02 -0800

    First Commit
    Unchanged Bootstrap set up, except for the added drop down menus and
    info added to jumbotron

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('#slbutton').click(function(){
+      $('#sllist').toggleClass('hidden');
+      });
+     $('#hlbutton').click(function(){
+      $('#hllist').toggleClass('hidden');
+      });
+     $('#opbutton').click(function(){
+      $('#oplist').toggleClass('hidden');
+      });
+});
